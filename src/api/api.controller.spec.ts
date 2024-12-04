b0b5bd3c7f94368b43c30d70db16796a22c8240f
dawid-makowskki <dawid.makowski.github@gmail.com>
2024-12-05 00:33:36 +0100

    feat: add pagination tests, update endpoint tests

--- src/api/api.controller.spec.ts
+++ src/api/api.controller.spec.ts
@@ -21,7 +21,7 @@ describe('AppController', () => {
   });
 
   describe('getAll', () => {
-    it('should return valid data for each endpoint', () => {
+    it('should return valid data for each endpoint', async () => {
       const controllerMethods = [
         'getAllFilms',
         'getAllPlanets',
@@ -30,29 +30,39 @@ describe('AppController', () => {
         'getAllVehicles',
       ];
       const promises = controllerMethods.map((methodName) =>
-        apiController[methodName](),
+        apiController[methodName]({}, 0),
       );
-      Promise.allSettled(promises).then((results) => {
-        results.forEach((result) => expect(result).toBe('fulfilled'));
-      });
+      const results = await Promise.allSettled(promises);
+
+      results.forEach((result) => expect(result.status).toBe('fulfilled'));
     });
   });
 
   describe('getOne', () => {
-    it('should return valid data for each endpoint', () => {
+    it('should return valid data for each endpoint', async () => {
       const controllerMethods = [
-        'getFilm',
-        'getPlanet',
-        'getSpecies',
-        'getStarship',
-        'getVehicle',
+        ['getFilm', '1'],
+        ['getPlanet', '1'],
+        ['getSpecies', '1'],
+        ['getStarship', '9'],
+        ['getVehicle', '4'],
       ];
-      const promises = controllerMethods.map((methodName) =>
-        apiController[methodName]('1'),
+      const promises = controllerMethods.map((method) =>
+        apiController[method[0]](method[1]),
       );
-      Promise.allSettled(promises).then((results) => {
-        results.forEach((result) => expect(result).toBe('fulfilled'));
-      });
+
+      const results = await Promise.allSettled(promises);
+
+      results.forEach((result) => expect(result.status).toBe('fulfilled'));
+    });
+  });
+
+  describe('pagination', () => {
+    it('should return correct paginated response with two items', async () => {
+      const response = await apiController.getAllFilms({}, 2);
+      expect(response.pages).toBe(3);
+      expect(response.page).toBe(2);
+      expect(response.data.length).toBe(2);
     });
   });
 });

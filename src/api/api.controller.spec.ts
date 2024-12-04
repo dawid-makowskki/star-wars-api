1de67327b490fc05748206d32df137f3785a7e53
dawid-makowskki <dawid.makowski.github@gmail.com>
2024-12-04 22:19:47 +0100

    feat: add simple tests for all endpoinds in apiService

--- src/api/api.controller.spec.ts
+++ src/api/api.controller.spec.ts
@@ -16,7 +16,43 @@ describe('AppController', () => {
 
   describe('root', () => {
     it('should return "Hello World!"', () => {
-      expect(apiController.healthCheck()).toBe('Hello World!');
+      expect(apiController.healthCheck()).toBe('Hello world');
+    });
+  });
+
+  describe('getAll', () => {
+    it('should return valid data for each endpoint', () => {
+      const controllerMethods = [
+        'getAllFilms',
+        'getAllPlanets',
+        'getAllSpecies',
+        'getAllStarships',
+        'getAllVehicles',
+      ];
+      const promises = controllerMethods.map((methodName) =>
+        apiController[methodName](),
+      );
+      Promise.allSettled(promises).then((results) => {
+        results.forEach((result) => expect(result).toBe('fulfilled'));
+      });
+    });
+  });
+
+  describe('getOne', () => {
+    it('should return valid data for each endpoint', () => {
+      const controllerMethods = [
+        'getFilm',
+        'getPlanet',
+        'getSpecies',
+        'getStarship',
+        'getVehicle',
+      ];
+      const promises = controllerMethods.map((methodName) =>
+        apiController[methodName]('1'),
+      );
+      Promise.allSettled(promises).then((results) => {
+        results.forEach((result) => expect(result).toBe('fulfilled'));
+      });
     });
   });
 });
